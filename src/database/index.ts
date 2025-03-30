1fc0768d2fc4fc066e65cbf964c9c29a404165eb
bosiiipo <olabosiposogbolu@gmail.com>
2025-03-30 12:10:57 +0100

    DBTransaction()

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,5 @@
-import mongoose from 'mongoose';
+import mongoose, { ClientSession, Connection } from 'mongoose';
+import { InternalError, UnprocessableEntityException } from '../responses/errors';
 import {config} from '../config';
 
 export const connectMongoose = async () => {
@@ -9,3 +10,55 @@ export const connectMongoose = async () => {
     console.log(error);
   }
 };
+
+const maxWriteConflictRetry = 2;
+let retry = 0;
+
+export const dbTransaction = async <T>(
+  connection: Connection,
+  cb: (session: ClientSession) => Promise<T>,
+): Promise<T> => {
+  const session = await connection.startSession();
+  try {
+    session.startTransaction();
+    const result = await cb(session);
+    await session.commitTransaction();
+    return result;
+  } catch (err: any) {
+    await session.abortTransaction();
+    console.error('db_transaction_error', JSON.stringify(err));
+    if (err instanceof mongoose.mongo.MongoError) {
+      switch (err.code) {
+        case 8: //unknown error
+        case 112: //WriteConflict
+        case 117: {
+          //ConflictingOperationInProgress
+          //retying for write conflicts and unknown errors
+          if (retry <= maxWriteConflictRetry) {
+            ++retry;
+            console.error('db_transaction_error_retry');
+            return dbTransaction(connection, cb);
+          }
+          throw new UnprocessableEntityException('Unable to process transaction, please try again');
+        }
+
+        case 11000: {
+          //unique constraint error
+          throw new UnprocessableEntityException(
+            'Duplicate reference detected, please try again with a new reference',
+          );
+        }
+        //unexpected error, client to treat as pending
+        default: {
+          throw new InternalError('Unexpected error occurred', err);
+        }
+      }
+    }
+
+    throw err;
+  } finally {
+    await session.endSession();
+  }
+};
+
+
